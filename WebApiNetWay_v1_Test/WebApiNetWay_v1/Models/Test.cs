eb76cfedd8ea7c26898058316d774d2f9268b5f3
xuanducvx <xuanducvx.firebase@gmail.com>
2023-04-28 15:50:05 +0700

    them model test

---
+++ WebApiNetWay_v1_Test/WebApiNetWay_v1/Models/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiNetWay_v1.Models
+{
+    public class Test
+    {
+        public string code { set; get; }
+        public string name { set; get; }
+
+
+    }
+}
